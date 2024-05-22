5620a706af5c2d982f3049c58c5d27423b148fd1
DJaegerScript <adjiedjaka0902@gmail.com>
2024-05-22 11:21:24 +0700

    fixe whitelist url

--- server/src/handler/index.ts
+++ server/src/handler/index.ts
@@ -3,7 +3,9 @@ import { getAllJobs, getCompanies, getLocations } from './job.handler'
 import cors from 'cors'
 
 export const initHandler = (app: Express) => {
-  app.use(cors({ origin: 'http://localhost:3000, https://tk-eai.vercel.app' }))
+  app.use(
+    cors({ origin: ['http://localhost:3000, https://tk-eai.vercel.app'] })
+  )
 
   app.get('/', getAllJobs)
   app.get('/companies', getCompanies)
