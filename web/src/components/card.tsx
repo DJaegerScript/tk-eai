2d21778dfc53aac5df4e77e146bfb9a1b7d78375
DJaegerScript <adjiedjaka0902@gmail.com>
2024-05-22 10:55:04 +0700

    fix unmatch overload and remove commented code

--- web/src/components/card.tsx
+++ web/src/components/card.tsx
@@ -1,28 +1,36 @@
-import { JobPostingAttributes } from "@/lib/model";
-import { FC } from "react";
+import { JobPostingAttributes } from '@/lib/model';
+import { FC } from 'react';
 
-export const Card: FC<JobPostingAttributes> = ({ profession, company, location, source, title, url, date }) => {
+export const Card: FC<JobPostingAttributes> = ({
+  profession,
+  company,
+  location,
+  source,
+  title,
+  url,
+  date,
+}) => {
   return (
-    <div className="card-container">
-      <div className="card">
-        <a className="card1" href={url}>
-          <span className="badge badge-light">{profession}</span>  <p className="title font-semibold mt-2">{title}</p>  
-          <p className="small">Perusahaan : <span>{company}</span>
+    <div className='card-container'>
+      <div className='card'>
+        <a className='card1' href={url}>
+          <span className='badge badge-light'>{profession}</span>{' '}
+          <p className='title font-semibold mt-2'>{title}</p>
+          <p className='small'>
+            Perusahaan : <span>{company}</span>
           </p>
-          
-          <p className="small">
-          Lokasi : <span>{location}</span>
-          </p >
-          <p className="small">
-              Tanggal Publikasi : <span>{new Date(date).toLocaleDateString()}</span>
+          <p className='small'>
+            Lokasi : <span>{location}</span>
           </p>
-          <p className="small">
-          Sumber : <span>{source}</span>
+          <p className='small'>
+            Tanggal Publikasi :{' '}
+            <span>{new Date(date || Date.now()).toLocaleDateString()}</span>
           </p>
-          <a className="go-corner" href={url}>
-            <div className="go-arrow">
-              →
-            </div>
+          <p className='small'>
+            Sumber : <span>{source}</span>
+          </p>
+          <a className='go-corner' href={url}>
+            <div className='go-arrow'>→</div>
           </a>
         </a>
       </div>
