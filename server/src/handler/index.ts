1d779e05a86cc1baf6fea57e4075618d7e4c3b24
DJaegerScript <adjiedjaka0902@gmail.com>
2024-05-22 11:27:37 +0700

    try different approach

--- server/src/handler/index.ts
+++ server/src/handler/index.ts
@@ -1,11 +1,22 @@
 import { Express } from 'express'
 import { getAllJobs, getCompanies, getLocations } from './job.handler'
-import cors from 'cors'
 
 export const initHandler = (app: Express) => {
-  app.use(
-    cors({ origin: ['http://localhost:3000, https://tk-eai.vercel.app'] })
-  )
+  app.use((req, res, next) => {
+    const allowedOrigins = [
+      'http://127.0.0.1:3000',
+      'http://localhost:3000',
+      'https://tk-eai.vercel.app',
+    ]
+    const origin = req.headers.origin
+    if (allowedOrigins.includes(origin!)) {
+      res.setHeader('Access-Control-Allow-Origin', origin!)
+    }
+    res.header('Access-Control-Allow-Methods', 'GET, OPTIONS')
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    res.header('Access-Control-Allow-Credentials', 'true')
+    return next()
+  })
 
   app.get('/', getAllJobs)
   app.get('/companies', getCompanies)
